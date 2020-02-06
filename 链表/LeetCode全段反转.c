2ab4a0c2bbe9981d6141b98565a0d3320de4318e
ConnorTomato <57027969+ConnorTomato@users.noreply.github.com>
2020-02-06 13:23:21 +0800

    Add files via upload

---
+++ 链表/LeetCode全段反转.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct ListNode{
+    int Data;
+    struct ListNode*next;
+};
+
+struct ListNode*Creat(int n){
+    struct ListNode*head,*node,*end;
+    head = (struct ListNode*)malloc(sizeof(struct ListNode));
+    end = head;
+    for(int i = 0; i<n; i++)
+    {
+        node = (struct ListNode*)malloc(sizeof(struct ListNode));
+        scanf("%d",&node->Data);
+        end->next = node;
+        end = node;
+    }
+    end->next = NULL;
+    return head;
+}
+
+
+struct ListNode* reverseList(struct ListNode* head){
+    
+    struct ListNode * res = NULL, *cur = head;
+    while (head != NULL){
+        head = head->next;
+        cur->next = res;
+        res= cur;
+        cur = head;
+
+    }
+    return res;
+}
+void Print(struct ListNode*head){
+    struct ListNode*p = head;
+    p = p->next;
+    while(p){
+        printf("%d\t",p->Data);
+        p = p->next;
+    }
+}
+int main(){
+    struct ListNode*head;
+    int n;
+    int N;
+    scanf("%d",&n);
+    head = Creat(n);
+    Print(head);
+    printf("\n");
+    head = reverseList(head);
+    Print(head);
+}
