bcf69a325ac118e9ab86af910afeed5a17802350
ConnorTomato <57027969+ConnorTomato@users.noreply.github.com>
2020-02-27 22:03:48 +0800

    Create 110.平衡二叉树.java

---
+++ 二叉树/LeetCode/难度：简单/110.平衡二叉树.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        //递归计算每个节点的左右子树高度
+        if(!isBalanced(root.left)) return false;
+        if(!isBalanced(root.right)) return false;
+
+        int leftH = dfs(root.left) + 1;     //计算左子树高度
+        int rightH = dfs(root.right) + 1;   //计算右子树高度
+        if (Math.abs(leftH - rightH) > 1) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     *   算出每个节点的子树高度
+     */
+    private int dfs(TreeNode x) {
+        if (x == null) {
+            return 0;
+        }
+        if (x.left == null && x.right == null) {
+            return 1;
+        }
+        int leftH = dfs(x.left) + 1;
+        int rightH = dfs(x.right) + 1;
+        return Math.max(leftH, rightH);
+    }
+}
