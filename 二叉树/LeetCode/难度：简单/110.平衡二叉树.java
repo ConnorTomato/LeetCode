0a61f4b5dc7aeef36fab7a9b3f39c1d22c1753af
ConnorTomato <57027969+ConnorTomato@users.noreply.github.com>
2020-03-01 10:48:48 +0800

    Update 110.平衡二叉树.java

--- 二叉树/LeetCode/难度：简单/110.平衡二叉树.java
+++ 二叉树/LeetCode/难度：简单/110.平衡二叉树.java
@@ -46,3 +46,24 @@ class Solution {
             return -1;        
     }
 }
+Ⅲ.递归
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+            if( root == null )
+                return true;
+            int leftH = treeH(root.left);
+            int rightH = treeH(root.right);
+            if(Math.abs(leftH - rightH) > 1)
+                return false;
+            return isBalanced(root.left) && isBalanced(root.right);
+            
+    }
+
+    private int treeH(TreeNode root){
+        if(root == null)
+            return 0;
+        int leftH = treeH(root.left) ;
+        int rightH = treeH(root.right);
+        return Math.max(leftH,rightH) + 1;
+    }
+}
