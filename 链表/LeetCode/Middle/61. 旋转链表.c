cd47d5991f7792b6b0d72310d17a4414dcfc018f
ConnorTomato <57027969+ConnorTomato@users.noreply.github.com>
2020-03-02 13:04:47 +0800

    Update 61. 旋转链表.c

--- 链表/LeetCode/Middle/61. 旋转链表.c
+++ 链表/LeetCode/Middle/61. 旋转链表.c
@@ -1,4 +1,5 @@
- struct ListNode* rotateRight(struct ListNode* head, int k){
+Ⅰ. 
+struct ListNode* rotateRight(struct ListNode* head, int k){
      if(k == 0 || head == NULL)
         return head;
     struct ListNode*tra = head,*H = head;
@@ -15,4 +16,25 @@
         H = head->next;
         head->next = NULL;
         return H;
+}
+Ⅱ.JAVA
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+            if(head == null ||(head !=null && head.next == null))
+                return head;
+            ListNode travle = head;
+            int count = 1;
+            while(travle.next != null){
+                count++;
+                travle = travle.next;
+            }
+            travle.next = head;
+            ListNode cur = head;
+            for(int i = 1;i < count - k%count; i++){
+                    cur = cur.next;
+            }
+            ListNode result = cur.next;
+            cur.next = null;
+            return result;
+    }
 }
