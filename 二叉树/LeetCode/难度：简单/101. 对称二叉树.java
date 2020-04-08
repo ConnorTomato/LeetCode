03970f8f81ec85273c340b8b282892bd3c4d258d
ConnorTomato <57027969+ConnorTomato@users.noreply.github.com>
2020-04-08 16:57:36 +0800

    Update 101. 对称二叉树.java

--- 二叉树/LeetCode/难度：简单/101. 对称二叉树.java
+++ 二叉树/LeetCode/难度：简单/101. 对称二叉树.java
@@ -1,7 +1,16 @@
-class Solution {
+#### [101. 对称二叉树](https://leetcode-cn.com/problems/symmetric-tree/)
+
+> Knowledge Point:
+>
+> - 递归
+>
+> The number of rview : 1th;当前递归的任务是判断这一层节点值是否相等，返回的是boolean
+
+
+```java
     public boolean isSymmetric(TreeNode root) {
         if(root == null) return true;
-        else return isSame(root.left,root.right);
+        return isSame(root.left,root.right);
     }
 
     public boolean isSame(TreeNode root1,TreeNode root2){
@@ -9,4 +18,6 @@ class Solution {
         if(root1==null || root2==null) return false;
         return (root1.val == root2.val) && isSame(root1.left,root2.right)&&isSame(root1.right,root2.left);
     }
-}
+```
+
+
