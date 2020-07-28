7d9e4afb7eec6fed06a3396011d5c47bfebb9bb9
ConnorTomato <57027969+ConnorTomato@users.noreply.github.com>
2020-07-28 08:42:32 +0800

    Update 104. 二叉树的最大深度.c

--- 二叉树/LeetCode/难度：简单/104. 二叉树的最大深度.c
+++ 二叉树/LeetCode/难度：简单/104. 二叉树的最大深度.c
@@ -1,17 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
- */
+#### [104. 二叉树的最大深度](https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/)
 
+> 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户·
 
-int maxDepth(struct TreeNode* root){
-    if(root == NULL)
-        return 0;
-    int left =maxDepth(root->left);
-    int right = maxDepth(root->right);
-    return left > right ?  left+1 :right+1; 
+```java
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        return Math.max(maxDepth(root.left),maxDepth(root.right)) + 1;
+    }
 }
+
+
+
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+    if(root == null) return 0;
+    int leftDepth = maxDepth(root.left);
+    int rightDepth = maxDepth(root.right);
+    return Math.max(leftDepth,rightDepth)+1;
+    }
+}
+```
+
