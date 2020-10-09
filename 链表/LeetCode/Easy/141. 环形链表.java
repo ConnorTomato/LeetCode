6c6d7ff9449c3958ae7a243136fde5236d2377ca
JUli Zhang <57027969+ConnorTomato@users.noreply.github.com>
2020-10-09 20:15:06 +0800

    Update and rename 判是否有环.c to 141. 环形链表.java

---
+++ 链表/LeetCode/Easy/141. 环形链表.java
@@ -0,0 +1,24 @@
+#### [141. 环形链表](https://leetcode-cn.com/problems/linked-list-cycle/)
+
+> - 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+
+```java
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null ||head.next == null){
+            return false;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+```
+
