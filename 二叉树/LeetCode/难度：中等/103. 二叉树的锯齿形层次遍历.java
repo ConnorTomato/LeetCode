6703b21a5ba229e963d69923e3382f9632245c1d
JUli Zhang <57027969+ConnorTomato@users.noreply.github.com>
2020-12-22 15:25:18 +0800

    Update 103. 二叉树的锯齿形层次遍历.java

--- 二叉树/LeetCode/难度：中等/103. 二叉树的锯齿形层次遍历.java
+++ 二叉树/LeetCode/难度：中等/103. 二叉树的锯齿形层次遍历.java
@@ -1,25 +1,48 @@
+#### [103. 二叉树的锯齿形层序遍历](https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal/)
+
+> - 执行用时：1 ms, 在所有 Java 提交中击败了98.42%的用户
+
+```java
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
-        if(root == null) return res;
-        Deque<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
+        if(root == null){
+            return Collections.emptyList();
+        }
+        List<List<Integer>> res = new LinkedList<List<Integer>>();
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
         int depth = 0;
         while(!queue.isEmpty()){
-            List<Integer> tmp = new LinkedList<>();
-            int cnt = queue.size();
-            for(int i = 0; i < cnt; i++){
+            int size = queue.size();
+            LinkedList<Integer> temp = new LinkedList<Integer>();
+            for(int i = 0; i < size; i++){
                 TreeNode node = queue.poll();
-                if(depth%2 == 0) tmp.add(node.val);
-                else tmp.add(0,node.val);
-                if(node.left != null) 
-                queue.add(node.left);
-                if(node.right != null)
-                queue.add(node.right);
+                if((depth & 1) == 1){
+                    temp.addFirst(node.val);
+                }else{
+                    temp.add(node.val);
+                }
+                if(node.left != null){
+                    queue.offer(node.left);
+                }
+                if(node.right != null){
+                    queue.offer(node.right);
+                }
             }
-            res.add(tmp);
             depth++;
+            res.add(temp);
         }
         return res;
     }
 }
+```
+
